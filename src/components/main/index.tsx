7a3bdab0898f93f0d9350cd5cc0200770b93be37
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-25 04:38:00 -0300

    header structure

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -4,13 +4,73 @@ import { Button } from 'components/button'
 /* ------| Styles |------ */
 import { Codepen, GitHub, Linkedin, Search } from 'react-feather'
 import {
+  Form,
+  FormContent,
+  FormInput,
+  Header,
+  Social,
+  SocialList,
+  SocialListItem,
+  SocialListLink,
+  SocialListLinkAlt,
+  SocialListLinkAltText,
   Wrapper,
 } from './styles'
 
 export const Main = () => {
   return (
     <Wrapper>
-      Conteudo
+      <Header>
+        <Form>
+          <FormContent>
+            <Button
+              theme='icons'
+              type='button'
+            >
+              <Search size={20} />
+            </Button>
+            <FormInput
+              placeholder='Search for an icon'
+            />
+          </FormContent>
+        </Form>
+        <Social>
+          <SocialList>
+            <SocialListItem>
+              <SocialListLink
+                href='https://github.com/meluiz'
+                target='_blank'
+              >
+                <GitHub size={20} />
+              </SocialListLink>
+            </SocialListItem>
+            <SocialListItem>
+              <SocialListLink
+                href='https://codepen.io/meluiz'
+                target='_blank'
+              >
+                <Codepen size={20} />
+              </SocialListLink>
+            </SocialListItem>
+            <SocialListItem>
+              <SocialListLink
+                href='https://www.linkedin.com/in/luiz-felipe-133b4a206/'
+                target='_blank'
+              >
+                <Linkedin size={20} />
+              </SocialListLink>
+            </SocialListItem>
+            <SocialListItem>
+              <SocialListLinkAlt
+                href='https://www.linkedin.com/in/luiz-felipe-133b4a206/'
+                target='_blank'
+              >
+                <SocialListLinkAltText>meluiz.com</SocialListLinkAltText>
+              </SocialListLinkAlt>
+            </SocialListItem>
+          </SocialList>
+        </Social>
+      </Header>
     </Wrapper>
   )
 }
