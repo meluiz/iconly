f21a599ba3e731b777f02e303b6f7a664527b35a
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-25 03:47:04 -0300

    content  style structure

--- src/components/main/styles.ts
+++ src/components/main/styles.ts
@@ -1,5 +1,11 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.aside`
+export const Wrapper = styled.aside`${({ theme }) => css`
   grid-area: content;
-`
+  display: flex;
+  flex-direction: column;
+  flex: content;
+  position: relative;
+  overflow: hidden;
+  background-color: ${theme.colors.background.primary};
+`}`
