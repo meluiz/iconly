9033f96cb321c5ad44ec46192e318f645dfe3cb1
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-25 01:20:24 -0300

    Typing theme

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,14 @@
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      primary: string;
+      secondary: string;
+      background: {
+          primary: string;
+          secondary: string;
+      }
+    }
+  }
+}
+
+export {}
