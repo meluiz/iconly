76febeb11cd20603e33859f76d1b02146799fbb2
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-26 00:57:55 -0300

    handling SidebarMenu stats

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent, useState } from 'react'
+
 /* ------| Hooks |------ */
 import { useIcons } from 'hooks/useIcons'
 
@@ -9,19 +11,28 @@ import { Sidebar } from 'components/sidebar'
 import { Container, Wrapper } from 'styles/layout'
 
 export function App () {
+  const [menuActived, setMenuActived] = useState<boolean>(false)
   const {
     categories,
     handleActiveCategory,
   } = useIcons()
 
+  const handleSidebarMenu = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    setMenuActived(actived => !actived)
+  }
+
   return (
     <Wrapper>
       <Container>
         <Sidebar
+          menuActived={menuActived}
           categories={categories}
           handleActiveCategory={handleActiveCategory}
+          handleSidebarMenu={handleSidebarMenu}
         />
         <Main
+          handleSidebarMenu={handleSidebarMenu}
           category={categories.find(category => category.isActived === true)}
         />
       </Container>
