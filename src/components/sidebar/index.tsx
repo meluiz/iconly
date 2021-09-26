08f1b3add7641627a63a78983193d88bba0d41a8
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-26 15:59:19 -0300

    removing white space

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -93,7 +93,6 @@ export const Sidebar = ({
                   <NavigationListLinkCounter>{category.content.length}</NavigationListLinkCounter>
                 </NavigationListLink>
               </NavigationListItem>
-
             )}
           </NavigationList>
         </NavigationGroup>
