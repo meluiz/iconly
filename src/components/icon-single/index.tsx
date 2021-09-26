512343d49c72bce610e3310c6b40ce9d7a84ffcf
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-26 20:29:23 -0300

    creating IconSingle components

---
+++ src/components/icon-single/index.tsx
@@ -0,0 +1,15 @@
+/* ------| Styles |------ */
+import {
+  Container,
+  Wrapper,
+} from './styles'
+
+export const IconSingle = () => {
+  return (
+    <Wrapper>
+      <Container>
+        Olá
+      </Container>
+    </Wrapper>
+  )
+}
