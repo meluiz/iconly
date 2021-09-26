e9b4805219b04b73757d0aac4797fbb7dbba7f7b
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-26 00:10:10 -0300

    typing icons and categories

--- src/hooks/useIcons.tsx
+++ src/hooks/useIcons.tsx
@@ -1,1 +1,21 @@
 import Categories from 'data/icons.json'
+
+/* ------| Typing |------ */
+export type IconsType = {
+  name: string
+  icon: string
+  isActived: boolean
+  slug: string
+  tag: null
+  slugParent: string
+}
+
+export type CategoriesType = {
+    name: string
+    slug: string
+    isActived: boolean
+    url: string
+    icon: string
+    total: number
+    content: IconsType[]
+}
