00709038c838bd26547b3631b4d1310fae006376
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-27 03:34:09 -0300

    changing the structure to show activated icon

--- src/components/icon-single/index.tsx
+++ src/components/icon-single/index.tsx
@@ -27,94 +27,141 @@ import {
 
 export const IconSingle = () => {
   return (
-    <Wrapper>
-      <Container>
-        <Box>
-          <BoxHeader>
-            <BoxIcon>
-              <BoxIconName>Anchor</BoxIconName>
-              <BoxIconBadge>Free</BoxIconBadge>
-            </BoxIcon>
-            <Button
-              theme='icons'
-              type='button'
+        <Wrapper>
+          <Container
+            onClick={handleDeleteActivedIcon}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Box
+              onClick={handleStopPropagation}
+              initial={{ left: 512 }}
+              animate={{ left: 0 }}
+              exit={{ left: 512 }}
             >
-              <X size={20} />
-            </Button>
-          </BoxHeader>
-          <IconPreviewBox>
-            <IconPreview>
-              <IconPreviewExposure>
-                <IconPreviewArt size='sm' opactiy={0.3}>
-                  <Anchor />
-                </IconPreviewArt>
-                <IconPreviewArt size='md' opactiy={0.5}>
-                  <Anchor />
-                </IconPreviewArt>
-                <IconPreviewArt size='base' opactiy={1}>
-                  <Anchor />
-                </IconPreviewArt>
-                <IconPreviewArt size='md' opactiy={0.5}>
-                  <Anchor />
-                </IconPreviewArt>
-                <IconPreviewArt size='sm' opactiy={0.3}>
-                  <Anchor />
-                </IconPreviewArt>
-              </IconPreviewExposure>
-              <IconPreviewSamples>
-                <IconPreviewSamplesItem>
-                  <Anchor size={20} />
-                </IconPreviewSamplesItem>
-                <IconPreviewSamplesItem>
-                  <Anchor size={20} />
-                </IconPreviewSamplesItem>
-                <IconPreviewSamplesItem>
-                  <Anchor size={20} />
-                </IconPreviewSamplesItem>
-                <IconPreviewSamplesItem>
-                  <Anchor size={20} />
-                </IconPreviewSamplesItem>
-                <IconPreviewSamplesItem>
-                  <Anchor size={20} />
-                </IconPreviewSamplesItem>
-                <IconPreviewSamplesItem>
-                  <Anchor size={20} />
-                </IconPreviewSamplesItem>
-                <IconPreviewSamplesItem>
-                  <Anchor size={20} />
-                </IconPreviewSamplesItem>
-                <IconPreviewSamplesItem>
-                  <Anchor size={20} />
-                </IconPreviewSamplesItem>
-              </IconPreviewSamples>
-            </IconPreview>
-            <IconPreviewActions>
-              <Button
-                theme='text'
-                type='button'
-                icon={<Download />}
-              >
-                Download
-              </Button>
-              <Button
-                theme='text'
-                type='button'
-                icon={<Copy />}
-              >
-                Copy
-              </Button>
-            </IconPreviewActions>
-          </IconPreviewBox>
-          <IconInfo>
-            <IconInfoItem>
-              <IconInfoItemTitleBox>
-                <IconInfoItemTitle>Category</IconInfoItemTitle>
-              </IconInfoItemTitleBox>
-              <IconInfoItemText>Feather</IconInfoItemText>
-            </IconInfoItem>
-          </IconInfo>
-        </Box>
-      </Container>
-    </Wrapper>
+              <BoxHeader>
+                <BoxIcon>
+                  <BoxIconName>Anchor</BoxIconName>
+                  <BoxIconBadge>Free</BoxIconBadge>
+                </BoxIcon>
+                <Button
+                  theme='icons'
+                  type='button'
+                  onClick={handleDeleteActivedIcon}
+                >
+                  <X size={20} />
+                </Button>
+              </BoxHeader>
+              <IconPreviewBox>
+                <IconPreview>
+                  <IconPreviewExposure>
+                    <IconPreviewArt
+                      size='sm'
+                      opactiy={0.3}
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                    <IconPreviewArt
+                      size='md'
+                      opactiy={0.5}
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                    <IconPreviewArt
+                      size='base'
+                      opactiy={1}
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                    <IconPreviewArt
+                      size='md'
+                      opactiy={0.5}
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                    <IconPreviewArt
+                      size='sm'
+                      opactiy={0.3}
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                  </IconPreviewExposure>
+                  <IconPreviewSamples>
+                    <IconPreviewSamplesItem
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                    <IconPreviewSamplesItem
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                    <IconPreviewSamplesItem
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                    <IconPreviewSamplesItem
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                    <IconPreviewSamplesItem
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                    <IconPreviewSamplesItem
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                    <IconPreviewSamplesItem
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                    <IconPreviewSamplesItem
+                      dangerouslySetInnerHTML={{
+                        __html: iconActived.icon.icon,
+                      }}
+                    />
+                  </IconPreviewSamples>
+                </IconPreview>
+                <IconPreviewActions>
+                  <Button
+                    theme='text'
+                    type='button'
+                    icon={<Download />}
+                  >
+                    Download
+                  </Button>
+                  <Button
+                    theme='text'
+                    type='button'
+                    icon={<Copy />}
+                  >
+                    Copy
+                  </Button>
+                </IconPreviewActions>
+              </IconPreviewBox>
+              <IconInfo>
+                <IconInfoItem>
+                  <IconInfoItemTitleBox>
+                    <IconInfoItemTitle>Category</IconInfoItemTitle>
+                  </IconInfoItemTitleBox>
+                  <IconInfoItemText>{iconActived.category.name}</IconInfoItemText>
+                </IconInfoItem>
+              </IconInfo>
+            </Box>
+          </Container>
+        </Wrapper>
   )
 }
