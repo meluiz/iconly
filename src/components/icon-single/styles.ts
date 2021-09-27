7e24eb9cb52a83ab8a58aada27f67627b5f42b6a
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-27 01:48:16 -0300

    styling IconSingle components

--- src/components/icon-single/styles.ts
+++ src/components/icon-single/styles.ts
@@ -1,4 +1,9 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+type IconPreviewType = {
+  size: 'base' | 'md' | 'sm'
+  opactiy?: number
+}
 
 export const Wrapper = styled.div`
   width: 100%;
@@ -17,7 +22,163 @@ export const Container = styled.div`
   justify-content: flex-end;
   overflow: hidden;
   position: relative;
-  backdrop-filter: blur(4px);
   background-color: rgba(0,0,0,.6);
   transition: all 300ms ease;
+`
+
+export const Box = styled.div`${({ theme }) => css`
+  width: 100%;
+  max-width: 512px;
+  height: 100%;
+  position: relative;
+  background-color: ${theme.colors.background.secondary};
+  overflow: hidden;
+  overflow-y: auto;
+`}`
+
+export const BoxHeader = styled.div`
+  width: 100%;
+  height: 72px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  justify-content: space-between;
+  border-bottom: 1px solid rgba(255,255,255,.04);
+  padding: 0 40px;
+  position: relative;
+  `
+
+export const BoxIcon = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`
+
+export const BoxIconName = styled.h1`
+  overflow: hidden;
+  white-space: nowrap;
+  color: white;
+  font-size: 24px;
+  font-family: 'Circular', 'sans-serif';
+  `
+
+export const BoxIconBadge = styled.span`${({ theme }) => css`
+ border-radius: 999px;
+ padding: 8px 12px;
+ font-size: 14px;
+ color: white;
+ background: ${theme.colors.secondary};
+ text-shadow: 0 1px 0 rgba(0,0,0,.06);
+ font-weight: 600;
+ font-family: 'Circular', 'sans-serif';
+ `}`
+
+export const IconPreviewBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  padding: 24px 40px;
+  border-bottom: 1px solid rgba(255,255,255,.04);
+`
+
+export const IconPreview = styled.div`${({ theme }) => css`
+  overflow: hidden;
+  border-radius: 8px;
+  background-color: ${theme.colors.background.primary};
+`}`
+
+export const IconPreviewExposure = styled.div`
+  padding: 56px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+`
+
+export const IconPreviewArt = styled.div<IconPreviewType>`
+  color: white;
+  opacity: ${({ opactiy }) => opactiy || 1};
+
+  ${({ size }) => css`
+    ${size === 'sm' && css`
+      width: 24px;
+      height: 24px;
+    `}
+
+    ${size === 'md' && css`
+      width: 40px;
+      height: 40px;
+    `}
+
+    ${size === 'base' && css`
+      width: 72px;
+      height: 72px;
+    `}
+  `}
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+export const IconPreviewSamples = styled.div`
+  height: 56px;
+  display: grid;
+  grid-template-columns: minmax(0, 1fr) minmax(0, 1fr)
+                         minmax(0, 1fr) minmax(0, 1fr)
+                         minmax(0, 1fr) minmax(0, 1fr)
+                         minmax(0, 1fr) minmax(0, 1fr);
+`
+
+export const IconPreviewSamplesItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+
+  &:nth-child(1) { background-color: #F53D6B }
+  &:nth-child(2) { background-color: #FF7D52 }
+  &:nth-child(3) { background-color: #FFC233 }
+  &:nth-child(4) { background-color: #2DCA72 }
+  &:nth-child(5) { background-color: #00ACFF }
+  &:nth-child(6) { background-color: #EB459E }
+  &:nth-child(7) { background-color: #F75FDE }
+  &:nth-child(8) {
+    color: black;
+    background-color: white;
+  }
+`
+
+export const IconPreviewActions = styled.div`
+  display: grid;
+  grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
+  gap: 16px;
+`
+
+export const IconInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 24px 40px;
+  border-bottom: 1px solid rgba(255,255,255,.04);
+`
+
+export const IconInfoItem = styled.div`
+  display: flex;
+`
+
+export const IconInfoItemTitleBox = styled.div`
+  min-width: 106px;
+`
+
+export const IconInfoItemTitle = styled.span`
+  color: #ffffff;
+  font-weight: 600;
+`
+
+export const IconInfoItemText = styled.div`
+  display: flex;
+  color: rgba(255,255,255,.5);
 `
