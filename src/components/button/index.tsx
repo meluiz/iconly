964ed9c56d9830a94b1716b192e7e72a36b3db19
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-27 01:31:40 -0300

    add new theme to button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,19 +1,21 @@
 import { ReactNode, MouseEvent } from 'react'
 
 /* ------| Styles |------ */
-import { Base } from './styles'
+import { Base, BaseIcon } from './styles'
 
 /* ------| Types |------ */
 type ButtonType = {
   children: ReactNode
   type: 'button' | 'submit' | 'reset'
-  theme: 'icons'
+  theme: 'icons' | 'text'
+  icon?: ReactNode
   onClick?: (Event: MouseEvent<HTMLButtonElement>) => void
 }
 
 export const Button = ({
   children,
   theme,
+  icon,
   type = 'button',
   onClick,
 }: ButtonType) => {
@@ -23,6 +25,11 @@ export const Button = ({
       type={type}
       onClick={onClick}
     >
+      {icon && (
+        <BaseIcon>
+          {icon}
+        </BaseIcon>
+      )}
       {children}
     </Base>
   )
