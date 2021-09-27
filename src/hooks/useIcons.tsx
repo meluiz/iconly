a231338ff34b1414d5f2aabca722f3471989eb15
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-27 14:26:25 -0300

    giving feedback when copy

--- src/hooks/useIcons.tsx
+++ src/hooks/useIcons.tsx
@@ -122,6 +122,25 @@ export const useIcons = () => {
   const handleCopyActivatedIcon = (icon: IconsType) => (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
     navigator.clipboard.writeText(icon.icon)
+    toast(
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '4px',
+      }}
+      >
+        <svg width='24' height='24' fill='none' viewBox='0 0 24 24'>
+          <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.5' d='M5.75 12.8665L8.33995 16.4138C9.15171 17.5256 10.8179 17.504 11.6006 16.3715L18.25 6.75' />
+        </svg>  SVG Copied
+      </div>
+      , {
+        position: 'bottom-center',
+        closeButton: false,
+        className: 'iconly-toast iconly-toast-copy',
+        hideProgressBar: true,
+        autoClose: 3000,
+      })
   }
 
   return {
